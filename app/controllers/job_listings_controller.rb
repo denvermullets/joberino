722e43916a16e01938238f41955dbf63f30a358d
denvermullets <devmullets@gmail.com>
2021-01-31 18:48:49 -0500

    add comment, restart server

--- app/controllers/job_listings_controller.rb
+++ app/controllers/job_listings_controller.rb
@@ -3,6 +3,8 @@ class JobListingsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    # need to make this more efficient, can see it take more time as it has to
+    # go thru each loop
     all_jobs = JobListing.all
     ignored_jobs = UserJobListing.ignored_jobs(current_user.id)
     ignored_jobs_ids = ignored_jobs.map { |job| job.job_listing.id }
