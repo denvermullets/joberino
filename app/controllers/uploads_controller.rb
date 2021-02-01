0b75ff452c27d62b093e0c10ef9befeb412abd5e
denvermullets <devmullets@gmail.com>
2021-02-01 17:28:15 -0500

    :recycle: update styling for upload, add mini admin panel

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -2,9 +2,15 @@
 class UploadsController < ApplicationController
   before_action :authenticate_user!
 
+  def new_jobs
+    render :new_jobs, locals: { job_count: JobListing.count, user_count: User.count }
+  end
+
   def upload
     uploaded_io = params[:json]
     jobs = JSON.parse(File.read(uploaded_io))
     jobs.map { |job| JobListing.create(job) }
+
+    render :new_jobs, locals: { job_count: JobListing.count, user_count: User.count }
   end
 end
