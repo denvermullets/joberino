7e08ce54c041ff18780dc32e58c3ba041a27e8ee
denvermullets <devmullets@gmail.com>
2021-02-12 09:18:14 -0500

    :recycle: testing that vscode fixed double quote issue

--- app/javascript/controllers/ignore_job_controller.js
+++ app/javascript/controllers/ignore_job_controller.js
@@ -1,13 +1,13 @@
-import { Controller } from "stimulus"
+import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = ["ignoreButton"]
+  static targets = ['ignoreButton']
 
   hideJob(e) {
     e.preventDefault()
-    let jobMessage = document.getElementsByClassName("job-count")[0]
-    let jobCount = jobMessage.innerText.split(" ")[0] - 1
-    let jobTotal = jobMessage.innerText.split(" ")[5]
+    let jobMessage = document.getElementsByClassName('job-count')[0]
+    let jobCount = jobMessage.innerText.split(' ')[0] - 1
+    let jobTotal = jobMessage.innerText.split(' ')[5]
     e.currentTarget.parentNode.parentNode.parentNode.remove()
     jobMessage.innerText = `${jobCount} jobs displayed out of ${jobTotal} jobs posted this week`
   }
