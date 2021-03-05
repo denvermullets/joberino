b00bed4791f59601f69550aaad6883d64f8de80c
denvermullets <devmullets@gmail.com>
2021-03-05 06:30:49 -0500

    :sparkles: devise timeoutable add

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :confirmable, :lockable,  and :omniauthable
+  devise :database_authenticatable, :registerable, :timeoutable,
          :recoverable, :rememberable, :validatable, :trackable
 
   has_many :user_job_listings
